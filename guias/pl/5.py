c84e7dbf77fbc606baf0fcf9a00b7955a87b25d0
MarcosBianchii <marcosbianchi07@gmail.com>
2024-10-20 19:53:01 -0300

    sat

--- guias/pl/5.py
+++ guias/pl/5.py
@@ -1,41 +1,22 @@
 # (★) Implementar un modelo de programación lineal que resuelva el problema de 3-SAT mínimo: que encuentre una solución que satisfaga, utlizando la menor cantidad de variables en true posible.
 
 from pulp import LpProblem, LpVariable, LpMinimize, lpSum, value
-prob = LpProblem("min_3sat", LpMinimize)
 
 
-def OR(x, y):
-    global prob
-    res = LpVariable(f"({x.getName()} | {y.getName()})", cat="Binary")
-    prob += res == x + y
-    return res
+def min_sat(clausuras):
+    ys = {}  # O(n)
+    for clausura in clausuras:
+        for x in clausura:
+            absx = abs(x)
+            if absx not in ys:
+                ys[absx] = LpVariable(str(absx), cat="Binary")
 
+    prob = LpProblem("min SAT", LpMinimize)
+    prob += lpSum(ys.values())
 
-def AND(x, y):
-    global prob
-    res = LpVariable(f"({x.getName()} & {y.getName()})", cat="Binary")
-    prob += res <= x
-    prob += res <= y
-    prob += res == x + y - 1
-    return res
-
-
-def NOT(x):
-    global prob
-    res = LpVariable(f"!{x.getName()}", cat="Binary")
-    prob += res == 1 - x
-    return res
-
-
-def min_3sat(n, expression):
-    global prob
-    ys = [LpVariable(f"x{i}", cat="Binary") for i in range(n)]
-
-    prob += lpSum(ys)
-    prob += expression(*ys) == 1
+    # O(n)
+    for clausura in clausuras:
+        prob += lpSum(ys[x] if x > 0 else 1 - ys[-x] for x in clausura) >= 1
 
     prob.solve()
-    return [bool(value(y)) for y in ys]
-
-
-print(min_3sat(3, lambda x, y, z: OR(AND(x, z), AND(y, z))))
+    return [x for x, y in ys.items() if bool(value(y))]
