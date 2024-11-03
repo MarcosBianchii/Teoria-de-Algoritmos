1927789fd6b3e44f6f7f6ec283711a831988e3ff
MarcosBianchii <marcosbianchi07@gmail.com>
2024-11-03 20:25:43 -0300

    bt/3

--- guias/backtracking/3.py
+++ guias/backtracking/3.py
@@ -1,33 +1,33 @@
 # (★) Dado un tablero de ajedrez n×n, implementar un algoritmo por backtracking que ubique(si es posible) a n reinas de tal manera que ninguna pueda comerse con ninguna.
 
 def nreinas(n):
-    reinas = []
-
-    def espacio_libre(i, j):
-        for Ri, Rj in reinas:
+    def validar_nreinas(reinas):
+        Ui, Uj = reinas[-1]
+        for i in range(len(reinas) - 1):
+            Ri, Rj = reinas[i]
             # Columna
-            if Rj == j:
+            if Rj == Uj:
                 return False
 
             # Diagonales
-            if Ri - Rj == i - j or Ri + Rj == i + j:
+            if Ri - Rj == Ui - Uj or Ri + Rj == Ui + Uj:
                 return False
 
         return True
 
-    def nreinas_rec(i):
+    def bt(i, reinas):
         if i == n:
-            return True
+            return reinas
 
         for j in range(n):
-            if espacio_libre(i, j):
-                reinas.append((i, j))
-                if nreinas_rec(i + 1):
-                    return True
+            reinas.append((i, j))
+            if validar_nreinas(reinas):
+                bt(i + 1, reinas)
+                if len(reinas) == n:
+                    return reinas
 
-                reinas.pop()
+            reinas.pop()
 
-        return False
+        return reinas
 
-    nreinas_rec(0)
-    return reinas
+    return bt(0, [])
