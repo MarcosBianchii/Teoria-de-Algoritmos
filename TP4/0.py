084dc1fe7efcf8e14ea8fa3de65e8d6bfdc5a721
MarcosBianchii <marcosbianchi07@gmail.com>
2024-11-26 13:35:29 -0300

    nada

--- TP4/0.py
+++ TP4/0.py
@@ -12,12 +12,11 @@ def automata_al_menos_3_0s():
     a.estado("Q3", es_final=True)
 
     a.transicion_estado("Q0", "Q1", "0")
-    a.transicion_estado("Q1", "Q2", "0")
-    a.transicion_estado("Q2", "Q3", "0")
-    a.transicion_estado("Q3", "Q3", "0")
-
     a.transicion_estado("Q0", "Q0", "1")
+    a.transicion_estado("Q1", "Q2", "0")
     a.transicion_estado("Q1", "Q0", "1")
+    a.transicion_estado("Q2", "Q3", "0")
     a.transicion_estado("Q2", "Q0", "1")
+    a.transicion_estado("Q3", "Q3", "0")
     a.transicion_estado("Q3", "Q0", "1")
     return a
